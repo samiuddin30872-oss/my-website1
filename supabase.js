6680786125ea7393dca23e533b0eb8d4f89d47ef
samiuddin30872-oss <samiuddin30872@gmail.com>
2026-09-05 14:05:05 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
 // Raja Refrigeration Supabase configuration
 // Supabase publishable key
-export const SUPABASE_URL = "https://lijbsrhucqydlodzaimv.supabase.co";
+export const SUPABASE_URL = "https://lljbsrhucqydlodzaimv.supabase.co";
 export const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_WTb56ebS9m3UCon5x861cw_xUoEwUUx";
